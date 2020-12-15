2601b05791a9c97b7e4034aafde8e295caf76444
JeongWooYeong <wjd030811@gmail.com>
2020-12-15 15:39:42 +0900

    [FIX] 주말 예외처리 추가

--- default_attendance_scheduler.sql
+++ default_attendance_scheduler.sql
@@ -1,10 +1,18 @@
 SET GLOBAL event_scheduler = ON;
 
 DROP EVENT IF EXISTS default_attendance;
+DELIMITER $$
 CREATE EVENT IF NOT EXISTS default_attendance
     ON SCHEDULE
         EVERY 1 DAY STARTS CONCAT(CURDATE() + INTERVAL 1 DAY, ' ', '00:00:00')
     ON COMPLETION PRESERVE ENABLE
         COMMENT '기본 출석 데이터 삽입 스케줄러'
     DO
-        CALL CREATE_DEFAULT_ATTENDANCE(0)
+    BEGIN
+        DECLARE today INT DEFAULT DAYOFWEEK(CURDATE());
+
+        IF today != 1 AND today != 7 THEN
+            CALL CREATE_DEFAULT_ATTENDANCE(0);
+        END IF;
+    END $$
+DELIMITER ;
