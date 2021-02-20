316b19a5a9f71fbde46ebfa4fa0726e537511045
Jeong woo yeong <wjd030811@gmail.com>
2021-02-20 22:57:08 +0900

    Update default_attendance_scheduler.sql

--- default_attendance_scheduler.sql
+++ default_attendance_scheduler.sql
@@ -1,10 +1,9 @@
 SET GLOBAL event_scheduler = ON;
-
 DROP EVENT IF EXISTS default_attendance;
 DELIMITER $$
 CREATE EVENT IF NOT EXISTS default_attendance
     ON SCHEDULE
-        EVERY 1 DAY STARTS CONCAT(CURDATE() + INTERVAL 1 DAY, ' ', '15:00:00')
+        EVERY 1 DAY STARTS CONCAT(CURDATE(), ' ', '15:00:00')
     ON COMPLETION PRESERVE ENABLE
         COMMENT '기본 출석 데이터 삽입 스케줄러'
     DO
